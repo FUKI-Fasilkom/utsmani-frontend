c055bbff03f9b7ed36c6fe5b9f6ce76118bf242a
m.azzam <m.azzam.azis@gmail.com>
2024-11-05 10:25:01 +0700

    feat: testimony section done

--- src/modules/LandingPageModule/constant.ts
+++ src/modules/LandingPageModule/constant.ts
@@ -42,3 +42,27 @@ export const PROGRAM_EXAMPLES = [
     image_link: 'https://picsum.photos/100/100',
   },
 ]
+
+export const TESTIMONY_EXAMPLES = [
+  {
+    quote:
+      'Hafalan Al-Quran saya semakin banyak dan bacaan Al-Quran saya semakin lancar. Semoga Bimbingan Al-Quran di Utsmani semakin berkembang dan diminati banyak orang.',
+    nama: 'Muhammad Syauqi',
+    role: 'Kelas 4 sd, peserta tahsin...',
+    profilePicture: 'https://picsum.photos/100/100',
+  },
+  {
+    quote:
+      'Hafalan Al-Quran saya semakin banyak dan bacaan Al-Quran saya semakin lancar. Semoga Bimbingan Al-Quran di Utsmani semakin berkembang dan diminati banyak orang.',
+    nama: 'Muhammad Syauqi',
+    role: 'Kelas 4 sd, peserta tahsin...',
+    profilePicture: 'https://picsum.photos/100/100',
+  },
+  {
+    quote:
+      'Hafalan Al-Quran saya semakin banyak dan bacaan Al-Quran saya semakin lancar. Semoga Bimbingan Al-Quran di Utsmani semakin berkembang dan diminati banyak orang.',
+    nama: 'Muhammad Syauqi',
+    role: 'Kelas 4 sd, peserta tahsin...',
+    profilePicture: 'https://picsum.photos/100/100',
+  },
+]
