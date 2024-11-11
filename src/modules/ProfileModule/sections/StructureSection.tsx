dcc7e60a4e68a2f493238be384988b32682b0acb
ilhamelhamdi <ilhamabdillah123@gmail.com>
2024-11-11 23:31:13 +0700

    feat: structure section for profile page

--- src/modules/ProfileModule/sections/StructureSection.tsx
+++ src/modules/ProfileModule/sections/StructureSection.tsx
@@ -1,5 +1,18 @@
 import React from 'react'
+import Image from 'next/image'
 
 export const StructureSection = () => {
-  return <div>StructureSection</div>
+  return (
+    <section>
+      <h2 className="font-bold text-[36px] text-center text-[#6C4534]">
+        Struktur Organisasi
+      </h2>
+      <Image
+        src="/assets/images/struktur-organisasi.jpg"
+        width={2480}
+        height={3508}
+        alt="Struktur Organisasi"
+      />
+    </section>
+  )
 }
