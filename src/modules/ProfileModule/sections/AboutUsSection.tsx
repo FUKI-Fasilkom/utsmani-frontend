32c6b2d7ebdec6be7e1a8946f0b432cbbfed35cb
ilhamelhamdi <ilhamabdillah123@gmail.com>
2024-11-11 23:12:13 +0700

    feat: about us section for profile page

--- src/modules/ProfileModule/sections/AboutUsSection.tsx
+++ src/modules/ProfileModule/sections/AboutUsSection.tsx
@@ -1,5 +1,31 @@
 import React from 'react'
 
 export const AboutUsSection = () => {
-  return <div>AboutUsSection</div>
+  return (
+    <section className="container text-[#6C4534] mb-10">
+      <h1 className="font-bold text-[48px] text-center">Tentang Kami</h1>
+      <div className="drop-shadow-lg rounded-[20px] bg-white py-10 px-9 text-[18px] font-medium text-center flex flex-col gap-y-4">
+        <p>
+          Lembaga Bimbingan Al-Qur’an Al-Utsmani memulai pembelajaran Al-Quran
+          pada tahun 1415 H bertepatan dengan tahun 1995 M. Jumlah peserta didik
+          terus bertambah setiap tahun dan sudah banyak alumni yang belajar
+          tahsin, tajwid, dan tahfizh Al-Quran serta mengajar di berbagai
+          lembaga di seluruh Indonesia.
+        </p>
+        <p>
+          Pesantren Tahfizh Al-Quran Al-Utsmani memiliki harapan yang besar
+          kepada seluruh alumninya untuk mampu mengaplikasikan ilmunya di
+          kehidupan bermasyarakat, sehingga masyarakat mampu membaca Al-Quran
+          dengan baik dan benar terlebih mampu memampu memulai menghafal
+          Al-Utsmani. Setelah itu masyarakat diharapkan mampu istiqomah dalam
+          membaca Al-Quran agar memperoleh kebahagiaan di dunia dan akhirat.
+        </p>
+        <p>
+          Berangkat dari alasan tersebut, maka Lembaga Bimbingan Al-Quran
+          Al-Utsman berusaha semaksimal mungkin untuk mendidik pesertanya agar
+          memperoleh hasil yang maksimal.
+        </p>
+      </div>
+    </section>
+  )
 }
