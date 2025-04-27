c089666392ba2dc6886ba9db3b4993f2221f9d08
daffarafip24 <daffarafip24@gmail.com>
2025-04-27 13:35:28 +0700

    fix: Fix stupid bug, next can't build landing page

--- src/modules/LandingPageModule/sections/ProgramSection.tsx
+++ src/modules/LandingPageModule/sections/ProgramSection.tsx
@@ -16,20 +16,20 @@ export const ProgramSection: React.FC<ProgramSectionProps> = ({
       <div className="grid sm:grid-cols-2 lg:grid-cols-4 gap-y-8 gap-x-5">
         {programs.map((program: ProgramProps, index) => (
           <Link
-            href={`/program/${program.id}`}
+            href={`/program/${program?.id}`}
             key={index}
             className="w-[240px] h-[240px] border-2 border-brown rounded-[40px] overflow-hidden flex items-center justify-center relative"
           >
             <Image
-              src={program.cover_image}
-              alt={program.title}
+              src={program?.cover_image}
+              alt={program?.title}
               className="object-cover w-full h-full"
               width={288}
               height={272}
             />
             <div className="py-4 px-2 absolute bottom-0 w-full flex justify-center">
               <span className=" font-bold text-2xl text-center text-white1">
-                {program.title}
+                {program?.title}
               </span>
             </div>
           </Link>
