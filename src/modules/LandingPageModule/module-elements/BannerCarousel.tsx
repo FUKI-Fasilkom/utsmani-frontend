a2957de7f573800488cee57a8f7fa8d32e000a0c
Ilham Abdillah Alhamdi <ilham.abdillah@ui.ac.id>
2025-08-03 23:42:26 +0700

    feat: banner carousel in landing page

---
+++ src/modules/LandingPageModule/module-elements/BannerCarousel.tsx
@@ -0,0 +1,61 @@
+'use client'
+
+import Image from 'next/image'
+import Link from 'next/link'
+
+import {
+  Carousel,
+  CarouselContent,
+  CarouselDots,
+  CarouselItem,
+  CarouselNext,
+  CarouselPrevious,
+} from '@/components/ui/carousel'
+import Autoplay from 'embla-carousel-autoplay'
+
+interface Banner {
+  name: string
+  imageUrl: string
+  link: string | null
+}
+
+interface BannerCarouselProps {
+  banners: Banner[]
+}
+
+export const BannerCarousel: React.FC<BannerCarouselProps> = ({ banners }) => {
+  return (
+    <Carousel
+      className="w-full h-full relative"
+      opts={{ loop: true }}
+      plugins={[
+        Autoplay({
+          delay: 5000,
+          stopOnInteraction: false,
+        }),
+      ]}
+    >
+      <CarouselContent className="h-full">
+        {banners.map((banner, index) => (
+          <CarouselItem key={index} className="pl-0">
+            <Link
+              href={banner.link || '#'}
+              className="block relative w-full h-full"
+            >
+              <Image
+                src={banner.imageUrl}
+                alt={banner.name}
+                className="w-full h-full object-cover"
+                fill
+                priority={index === 0}
+              />
+            </Link>
+          </CarouselItem>
+        ))}
+      </CarouselContent>
+      <CarouselPrevious className="absolute left-4" />
+      <CarouselNext className="absolute right-4" />
+      <CarouselDots className="absolute bottom-4 inset-x-0 z-50" />
+    </Carousel>
+  )
+}
